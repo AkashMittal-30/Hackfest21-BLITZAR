8f1647d6e3c551e5bd6ff85552535aaa38cf0566
Bhimesh Agrawal <bhimeshagrawalggc@gmail.com>
2021-05-01 08:34:51 +0530

    added authentication and backend system

--- app.js
+++ app.js
@@ -9,3 +9,36 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+$("#signInBtn").click(function () {
+  email = $("#signInUserEmail").val()
+  password = $("#signInUserPassword").val()
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      alert("sign in successfull")
+      console.log(user);    // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+})
+$("#signUpBtn").click(function () {
+  email = $("#signUpUserEmail").val()
+  password = $("#signUpUserPassword").val()
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((cred) => {
+      // Signed in 
+      var user = cred.user;
+      console.log("User created Successfully")
+      return db.collection('users').doc(cred.user.uid).set({
+        name: $("#signUpUserName").val(),
+      }).catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          alert(errorMessage);
+          // ..
+        });
+    })
+})
